1ebaed5a4f4bf9859f0e04bcb5a56e5c2fc15f5b
Richard Widjaya <ricwidjaya@gmail.com>
2022-03-30 16:53:04 +0800

    feaT: add organization model

---
+++ migrations/20220330085000-create-organization.js
@@ -0,0 +1,38 @@
+'use strict'
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Organizations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      head_counts: {
+        type: Sequelize.INTEGER,
+        // The person who created it
+        defaultValue: 1
+      },
+      logo: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Organizations')
+  }
+}
